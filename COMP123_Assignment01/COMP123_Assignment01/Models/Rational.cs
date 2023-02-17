de065d3e60c4af01c94bd49e93010f394e39f113
nlaprade <123032857+nlaprade@users.noreply.github.com>
2023-02-17 18:00:25 -0500

    Add files via upload

---
+++ COMP123_Assignment01/COMP123_Assignment01/Models/Rational.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Assignment01.Models
+{
+    public class Rational
+    {
+        public int denominator;
+        public int numerator;
+
+        public int Denominator
+        {
+            get { return this.denominator; }
+            private set { this.denominator = value; }
+        }
+        public int Numerator
+        {
+            get { return this.numerator; }
+            private set { this.numerator = value; }
+        }
+        public Rational(int numerator = 0, int denominator = 1)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        public override string ToString()
+        {
+            return $"{this.numerator}/{this.denominator}";
+        }
+        public void IncreaseBy(Rational other)
+        {
+            if (this.denominator == other.denominator)
+            {
+                this.numerator += other.numerator;
+            }
+            else
+            {
+                numerator = (this.numerator * other.denominator) + (this.denominator * other.numerator);
+                denominator = (this.denominator * other.denominator);
+            }
+        }
+        public void DecreaseBy(Rational other)
+        {
+            if (this.denominator == other.denominator)
+            {
+                this.numerator -= other.numerator;
+            }
+            else
+            {
+                numerator = (this.numerator * other.denominator) - (this.denominator * other.numerator);
+                denominator = (this.denominator * other.denominator);
+            }
+        }
+    }
+}
---
+++ COMP123_Assignment01/COMP123_Assignment01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using COMP123_Assignment01.Models;
+namespace COMP123_Assignment01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational rational = new Rational(); // Zero Arugements
+
+
+            Rational rational1 = new Rational(1); // 1 Arguement
+
+
+            Rational rational2 = new Rational(1, 5); // 2 Arguements
+
+
+            Rational rational3 = new Rational(numerator: 4, denominator: 9); // Named Arguement
+
+
+            // 2 Pairs to be printed to Console:
+
+            Console.WriteLine($"{rational2} + {rational3} = ...");
+            rational2.IncreaseBy(rational3);
+            Console.WriteLine($"> {rational2}\n");
+
+            Console.WriteLine($"{rational3} - {rational1} = ...");
+            rational3.DecreaseBy(rational1);
+            Console.WriteLine($"> {rational3}\n");
+        }
+    }
+}
